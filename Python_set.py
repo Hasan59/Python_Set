ed65e89a8b0f23b32db95b975e63c19a0895cd53
Hasan Mahmud <1059hasan@gmail.com>
2022-06-14 23:41:42 +0600

    The Buriganga

--- Python_set.py
+++ Python_set.py
@@ -14,3 +14,44 @@ print(type(myset))
 
 thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
 print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+  thisset = {"apple", "banana", "cherry"}
+
+  print("banana" in thisset)
+
+  thisset = {"apple", "banana", "cherry"}
+
+  thisset.add("orange")
+
+  print(thisset)
+
+  thisset = {"apple", "banana", "cherry"}
+  tropical = {"pineapple", "mango", "papaya"}
+
+  thisset.update(tropical)
+
+  print(thisset)
+
+  thisset = {"apple", "banana", "cherry"}
+  mylist = ["kiwi", "orange"]
+
+  thisset.update(mylist)
+
+  print(thisset)
+
+  thisset = {"apple", "banana", "cherry"}
+
+  thisset.remove("banana")
+
+  print(thisset)
+
+  thisset = {"apple", "banana", "cherry"}
+
+  thisset.discard("banana")
+
+  print(thisset)
